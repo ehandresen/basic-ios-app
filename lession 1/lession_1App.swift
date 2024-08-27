1fc691e29c8393328cb729dd0a4d879d5e9321eb
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-27 10:43:30 +0200

    Initial Commit

---
+++ lession 1/lession_1App.swift
@@ -0,0 +1,17 @@
+//
+//  lession_1App.swift
+//  lession 1
+//
+//  Created by Eirik Andresen on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct lession_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
